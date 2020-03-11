fc13ef6cc0460b20f2ec6ca86b51e883af9e9182
Rachele Edwards <dot.com1472@gmail.com>
2020-03-11 13:55:54 -0600

    imported the article less in the css/less folder, also finished step one in js

--- Article/Article.js
+++ Article/Article.js
@@ -88,7 +88,7 @@ const data = [
   }
 ];
 
-/* Step 1: Create a function that creates a component. You will want your component to look like the template below: 
+/* Step 1: Create a function that creates a component. You will want your component to look like the template below: ✅
   
   <div class="article">
     <h2>{title of the article}</h2>
@@ -101,14 +101,44 @@ const data = [
 
   Hint: You will need to use createElement more than once here!
 
-  Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above.
+  Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above. ✅
 
   Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
 
-  Step 3: return the entire component.
+  Step 3: return the entire component. ✅
 
   Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
 
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
-
 */
+
+//Step 1: Create a function that creates a component. You will want your component to look like the template below: 
+//Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above.
+
+ function createArticle(data){
+  //create new elements
+  const article = document.createElement('div')
+  const h2 = document.createElement('h2')
+  const date = document.createElement('p')
+  const p1 = document.createElement('p')
+  const p2 = document.createElement('p')
+  const p3 = document.createElement('p')
+  const btn = document.createElement('span')
+
+  //setup the structure/parent:child of our elements
+  article.append(h2, date, p1, p2, p3, btn)
+
+  //add classes the elements
+  article.classList.add('article')
+  date.classList.add('date')
+  btn.classList.add('expandButton')
+
+  //add textContent
+  h2.textContent = data.title
+  date.textContent = data.date
+  p1.textContent = data.firstParagraph
+  p2.textContent = data.secondParagraph
+  p3.textContent = data.thirdParagraph
+
+    return article
+ }
